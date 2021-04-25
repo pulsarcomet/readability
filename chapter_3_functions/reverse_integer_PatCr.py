9b8ab341601140ac6904cb22b0d0feb9d99448ad
PatCR <rpatycr@gmail.com>
2021-04-25 15:05:05 +0900

    Refactoring 1 - 4

--- chapter_3_functions/reverse_integer_PatCr.py
+++ chapter_3_functions/reverse_integer_PatCr.py
@@ -21,6 +21,28 @@ Optional:
 
 """
 
+def reverse_integers(*numbers, ignore_sign=True, max_int_size=31):
+    """
+    :type x: int
+    :rtype: int
+    """
+
+    if (numbers):
+
+        reversed_int = []
+        for x in numbers:
+            s = math.copysign(1, x)
+            a    = float(str(abs(x))[::-1])
+            if 0 <= a < 2**max_int_size:
+                reversed_int.append( int(s*a) )
+            else:
+                reversed_int.append(0)
+        
+        return reversed_int
+    else:
+        return 0
+
+
 def reverse_integer(x):
     """
     :type x: int
@@ -37,17 +59,19 @@ def reverse_integer(x):
 # tests
 if __name__ == '__main__':
 
-    print(reverse_integer(51))
-    print(reverse_integer(-321))
     # should work before refactor
-    assert reverse_integer(51) == 15
-    assert reverse_integer(-321) == -123
-    assert reverse_integer(1463847412) == 2147483641
-    assert reverse_integer(2147483658) == 0
+    #assert reverse_integer(51) == 15
+    #assert reverse_integer(-321) == -123
+    #assert reverse_integer(1463847412) == 2147483641
+    #assert reverse_integer(2147483658) == 0
 
     # should work after refactor:
-    # assert list(reverse_integers(51)) == [15]
-    # assert list(reverse_integers(51, -321, 1463847412, 2147483658)) == [15, -123, 2147483641, 0]
+    print (reverse_integers(51))
+    assert list(reverse_integers(51)) == [15]
+    print (reverse_integers(51, -321, 1463847412, 2147483658))
+    assert list(reverse_integers(51, -321, 1463847412, 2147483658)) == [15, -123, 2147483641, 0]
+    assert reverse_integers() == 0
+    print (reverse_integers())
     # optional
-    # assert list(reverse_integers(range(1,30,10))) == [1, 11, 12]
+    #assert list(reverse_integers(range(1,30,10))) == [1, 11, 12]
 
